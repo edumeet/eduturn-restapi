3c02e91ec611de4720c8138c410ca1a0510c6095
Mészáros Mihály <misi@niif.hu>
2015-11-23 13:21:33 +0100

    geolocation

--- src/lib/Coturn.php
+++ src/lib/Coturn.php
@@ -24,13 +24,12 @@ class Coturn {
      *         maxLength=254,
      *     ),
      *     @SWG\Parameter(
-     *         description="client browser iso country",
+     *         description="client browser IPv4/IPv6 Address",
      *         in="query",
-     *         name="country",
+     *         name="ip",
      *         required=false,
      *         type="string",
-     *         minLength=2,
-     *         maxLength=2
+     *         maxLength=45
      *     ),
      *     @SWG\Response(
      *       response="200",
@@ -69,13 +68,12 @@ class Coturn {
      *         maxLength=254,
      *     ),
      *     @SWG\Parameter(
-     *         description="client browser iso country",
+     *         description="client browser IPv4/IPv6 Address",
      *         in="query",
-     *         name="country",
+     *         name="ip",
      *         required=false,
      *         type="string",
-     *         minLength=2,
-     *         maxLength=2
+     *         maxLength=45
      *     ),
      *     @SWG\Response(
      *       response="200",
@@ -100,46 +98,78 @@ class Coturn {
 
       try 
       {
-          /// TOKEN IS A PARAM VARIABLE
-          if ($app->request->params('api_key') == $app->token) {
-          /// TOKEN IS A HEADER VARIABLE
-          //if ($app->request->headers('api_key') == $app->token) { 
-
-            // response
-            $response=new ApiResponse();
-            $response->ttl=3600;
-            $response->username=time().":".$app->request->params('ufrag');
-
-            if ($app->request->params('realm')){ 
-                $realm=$app->request->params('realm');
-            }
-
-            $db = Db::Connection();
-            $sth = $db->prepare("SELECT value FROM turn_secret where realm='$realm' limit 1");
-            $sth->execute();
-            $data = $sth->fetchAll(PDO::FETCH_OBJ);
-            if($data[0]->value) {
-               $response->password=hash_hmac("sha1",$response->username,$data[0]->value);
-               $app->response->setStatus(200);
-               $app->response()->headers->set('Content-Type', 'application/json');
-               echo json_encode($response);
-               $db = null;
-            } else {
-                throw new PDOException('No records found.');
-            }
+         /// TOKEN IS A PARAM VARIABLE
+         if ($app->request->params('api_key') == $app->token) {
+         /// TOKEN IS A HEADER VARIABLE
+         //if ($app->request->headers('api_key') == $app->token) { 
 
 
-          } else {
-            $app->response->setStatus(404);
+
+         //connectdb
+         $db = Db::Connection();
+
+         //update not existing lat long in server table
+         $sth = $db->prepare("SELECT id,ip FROM servers where latitude IS NULL OR longitude IS NULL");
+         $sth->execute();
+         $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+         foreach ($result as $row => $columns) {
+            $location=$this->GetGeoIP($columns['ip']);
+            $sth2 = $db->prepare("UPDATE servers SET latitude=$location->latitude, longitude=$location->longitude WHERE id=$columns[id]");
+            $sth2->execute();
+         }
+
+         //check if realm presents
+         if ($app->request->params('realm')){ 
+             $realm=$app->request->params('realm');
+         }
+
+         // response
+         $response=new ApiResponse();
+         $response->ttl=3600;
+         $response->username=time().":".$app->request->params('ufrag');
+
+
+         $sth = $db->prepare("SELECT value FROM turn_secret where realm='$realm' limit 1");
+         $sth->execute();
+         $sharedsecret = $sth->fetchColumn();
+         if($sharedsecret) {
+            $response->password=hash_hmac("sha1",$response->username,$sharedsecret);
+            $app->response->setStatus(200);
             $app->response()->headers->set('Content-Type', 'application/json');
-            echo '{"error":{"text": "Invalid api_key" }}';
-          }
-   
+            echo json_encode($response);
+            $db = null;
+         } else {
+             throw new PDOException('No records found.');
+         }
+
+
+        } else {
+          $app->response->setStatus(404);
+          $app->response()->headers->set('Content-Type', 'application/json');
+          echo '{"error":{"text": "Invalid api_key" }}';
+        }
+ 
       } catch(PDOException $e) {
           $app->response()->setStatus(404);
           $app->response()->headers->set('Content-Type', 'application/json');
           echo '{"error":{"text": "'. $e->getMessage() .'"}}';
       }
+    }
+
+
+
+    private function GetGeoIP ($ip) {
+      $database = (strpos($ip, ":") === false) ? "GeoLiteCity.dat" : "GeoLiteCityv6.dat";
+      $gi = geoip_open("/usr/local/share/GeoIP/$database",GEOIP_STANDARD);
+      if((strpos($ip, ":") === false)) {
+          //ipv4
+          $record = geoip_record_by_addr($gi, $ip);
+      }
+      else {
+          //ipv6
+          $record = geoip_record_by_addr_v6($gi, $ip);
+      }
+      return $record;
     }
 
 }

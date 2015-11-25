2f26a63dfd626f43903686203ce5329998c99864
Mészáros Mihály <misi@niif.hu>
2015-11-25 13:54:37 +0100

    Bugfix: NOW + TTL and not NOW - TTL

--- src/lib/Coturn.php
+++ src/lib/Coturn.php
@@ -111,7 +111,7 @@ class Coturn {
          // response
          $response=new ApiResponse();
          $response->ttl=86400;
-         $response->username=(time() - $response->ttl).":".$app->request->params('ufrag');
+         $response->username=(time() + $response->ttl).":".$app->request->params('ufrag');
 
          //update not existing lat long in server table
          $sth = $db->prepare("SELECT id,ip FROM servers where latitude IS NULL OR longitude IS NULL");
